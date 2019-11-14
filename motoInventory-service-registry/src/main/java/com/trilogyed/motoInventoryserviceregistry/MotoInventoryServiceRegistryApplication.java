96b199851a84e6d00e952f624a538ff386ffd032
Brad Cas <bmcasalvieri@gmail.com>
2019-11-14 10:19:33 -0500

    service registry

--- motoInventory-service-registry/src/main/java/com/trilogyed/motoInventoryserviceregistry/MotoInventoryServiceRegistryApplication.java
+++ motoInventory-service-registry/src/main/java/com/trilogyed/motoInventoryserviceregistry/MotoInventoryServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.trilogyed.motoInventoryserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MotoInventoryServiceRegistryApplication {
 
 	public static void main(String[] args) {
